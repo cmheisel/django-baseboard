0a5104c4f5c3452094d8dbaed9bd991c2f31ad23
Chris Heisel <cheisel@Sherman.coxnews.net>
2009-04-09 16:02:33 -0400

    Added first stab at views/urls/templates.

---
+++ urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import *
+
+from django.views.generic.list_detail import object_list, object_detail
+
+from baseboard.models import Dashboard
+
+dashboard_list_args = {
+    'queryset': Dashboard.objects.all(),
+    'template_name': 'baseboard/dashboard_list.html',
+    'allow_empty': True,
+    'template_object_name': 'dashboard',
+}
+
+dashboard_detail_args = {
+    'queryset': Dashboard.objects,
+    'template_name': 'baseboard/dashboard_detail.html',
+    'slug_field': 'slug',
+}
+
+urlpatterns = patterns('',
+   url(r'^$', object_list, dashboard_list_args, name="dashboard_list"),
+   url(r'^dashboard/(?P<slug>[-\w]+/$)', object_detail, dashboard_detail_args, name="dashboard_detail"),
+)                      
