16aa15ba4b956dfe532e37060b5c545378504f54
Chris Heisel <cheisel@Sherman.coxnews.net>
2009-04-10 11:50:56 -0400

    Prettying up the admin.

--- admin.py
+++ admin.py
@@ -17,6 +17,15 @@ class ProjectAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_display=('name', 'updated_at', 'basecamp_updated_at', '_dashboard_count')
     list_filter=('updated_at', 'basecamp_updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ("basecamp_url", "description", "name", "slug"),
+        }),
+        ('Advanced options', {
+            'classes': ('collapse', ),
+            'fields': ('basecamp_id', 'readable_summary'),
+        }),
+    )
     
     def _dashboard_count(self, proj):
         return proj.dashboards.count()
