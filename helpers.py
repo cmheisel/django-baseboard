cd70778e11b28a33789f9d5701135a227851b494
Chris Heisel <cheisel@Sherman.coxnews.net>
2009-04-10 14:04:10 -0400

    Added management command.

--- helpers.py
+++ helpers.py
@@ -6,11 +6,15 @@ def update_summaries(debug):
     errors = []
     
     for p in Project.objects.all():
-        #try:
-        p.update_summary()
-        #except Exception, e:
-            #problems.append(e)
-            #errors.append("Error updating %s (%s): %s" % (p, p.id, str(e)))
+        try:
+            if debug == 2:
+                print "Updating %s (%s)" % (p.name, p.id)
+            p.update_summary()
+        except Exception, e:
+            if debug == 2:
+                print "ERROR updating %s (%s)" % (p.name, p.id)
+            problems.append(e)
+            errors.append("Error updating %s (%s): %s" % (p, p.id, str(e)))
 
     if errors and debug:
         print '\n'.join(errors)
