fec58418253aac54bc5c677d96013944e361b696
Chris Heisel <chris@heisel.org>
2009-04-16 16:27:59 -0400

    Fixed test settings.

--- src/baseboard/testsettings.py
+++ src/baseboard/testsettings.py
@@ -1,7 +1,7 @@
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = '/tmp/shorturls.db'
 INSTALLED_APPS = ['baseboard']
-ROOT_URLCONF = ['baseboard.urls']
+ROOT_URLCONF = 'baseboard.urls'
 BASEBOARD_CREDENTIALS = {
     'https://foo.basecamphq.com/': ('username1', 'password1')
-},
+}
