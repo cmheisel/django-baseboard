13ddcb721bdb82a1c12837f28c992351e16d2052
Chris Heisel <chris@heisel.org>
2009-04-24 10:27:59 -0400

    Adding settings for all test urls.

--- src/baseboard/testsettings.py
+++ src/baseboard/testsettings.py
@@ -3,5 +3,7 @@ DATABASE_NAME = '/tmp/shorturls.db'
 INSTALLED_APPS = ['baseboard']
 ROOT_URLCONF = 'baseboard.urls'
 BASEBOARD_CREDENTIALS = {
-    'https://foo.basecamphq.com/': ('username1', 'password1')
+    'https://foo.basecamphq.com/': ('username1', 'password1'),
+    'https://foo.updatelog.com/': ('username1', 'password1'),
+    'http://foo.updatelog.com/': ('username1', 'password1'),
 }
