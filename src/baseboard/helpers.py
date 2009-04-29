863407639075c66276e1f4ff8beea10e7bfdde01
Chris Heisel <chris@heisel.org>
2009-04-29 13:47:38 -0400

    Added upate command for feeds, refactored update command to be general.

--- src/baseboard/helpers.py
+++ src/baseboard/helpers.py
@@ -1,32 +1,47 @@
 """Common functions shared across the baseboard project."""
 import socket
 
-from baseboard.models import Project
+from baseboard.models import Project, RSSFeed
 
-def update_summaries(debug, timeout=20):
-    """Calls update_summary on all Project objects in the database."""
-    problems = []
-    errors = []
+def run_instance_update_method(queryset, method_name, debug, timeout=20):
+    """Loops through every object in queryset, and calls method_name.
+    If debug is true, it prints error messages to stdout
+    Optional timeout value is used to keep long-running network operations from stalling
+    the entire process.
+    """
+
+    exceptions = []
+    error_msgs = []
    
     old_timeout = socket.getdefaulttimeout() #Be kind
     socket.setdefaulttimeout(float(timeout))
-    for proj in Project.objects.all():
+    for obj in queryset:
+        method = getattr(obj, method_name)
         try:
             if debug == 2:
-                print "Updating %s (%s)" % (proj.name, proj.id)
-            proj.update_summary()
+                print "Updating %s" % (obj, )
+            method()
         except Exception, err:
+            msg = "ERROR updating %s => %s" % (obj, str(err))
             if debug == 2:
-                print "ERROR updating %s (%s)" % (proj.name, proj.id)
-            problems.append(err)
-            errors.append("Error updating %s (%s): %s" % 
-                    (proj, proj.id, str(err)))
+                print msg
+            exceptions.append(err)
+            error_msgs.append(msg)
+
     socket.setdefaulttimeout(old_timeout) #Rewind
 
-    if errors and debug:
-        print '\n'.join(errors)
+    if error_msgs and debug:
+        print '\n'.join(error_msgs)
         
-    if problems:
-        raise errors[0]
+    if exceptions:
+        raise exceptions[0]
 
     return True
+
+def update_summaries(debug, timeout=20):
+    """Calls update_summary on all Project objects in the database."""
+    run_instance_update_method(Project.objects.all(), 'update_summary', debug, timeout)
+
+def update_feeds(debug, timeout=20):
+    """Calls update_feed on all RSSFeed objects in the database."""
+    run_instance_update_method(RSSFeed.objects.all(), 'update_feed', debug, timeout)
